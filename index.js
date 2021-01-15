7f171f642cee909daeff200d977520b5d1546fe3
turpp <white.tripp@icloud.com>
2021-01-15 13:42:18 -0600

    commit tree f0853aeb0ea0a3fb96e66df034e8e35705a96451

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution here
+function findMatching(array, string){
+    //returns matching list of drivers and should be case insensitive
+   let matches=array.filter(function(driver){
+        return driver == string || driver.toLowerCase() == string.toLowerCase()
+    })
+    return matches
+}
+
+function fuzzyMatch(array, string){
+    let matches = array.filter(function(driver){
+        return driver[0].includes(string[0]) === true
+        
+    })
+    return matches
+}
+
+function matchName(array, string){
+    //each element in array is an object 
+    // return when string matches name property
+    let matches = array.filter(function(driver){
+        return driver.name === string
+    })
+    return matches
+}
+
+
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby']
